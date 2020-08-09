dd8b32b364d51ad7b8878c28ea64c783b7dfe73d
Ilia Kurganskiy <kurganskiy@mercurydevelopment.com>
2020-08-09 21:01:09 +0400

    build: add eslint

--- src/partials/sections/contact/contact.ts
+++ src/partials/sections/contact/contact.ts
@@ -1,39 +1,18 @@
 function sendEmail(event: Event) {
   event.preventDefault();
-
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (request.readyState == 4 && request.status == 200) {
-      //js_onSuccess();
-    } else if (request.readyState == 4) {
-      //js_onError(request.response);
-    }
-  };
-  let dataJS: any = {
-    access_token: "xa1j4squk3a1v4e1k6vy36ln",
-  };
-  dataJS["subject"] = "Subject";
-  dataJS["text"] = "Messages";
-  const params = toParams(dataJS);
-
-  request.open("POST", "https://postmail.invotes.com/send", true);
-  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-  request.send(params);
+  // fetch("https://postmail.invotes.com/send", {
+  //   method: "post",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     access_token: "xa1j4squk3a1v4e1k6vy36ln",
+  //     subject: "Subject",
+  //     text: "Messages",
+  //   }),
+  // }).then((r) => console.log(r));
 
   return false;
-}
-
-function toParams(dataJS: Object) {
-  var form_data = [];
-  for (let key in dataJS) {
-    // @ts-ignore
-    form_data.push(
-      encodeURIComponent(key) + "=" + encodeURIComponent(dataJS[key])
-    );
-  }
-
-  return form_data.join("&");
 }
 
 document.getElementById("contact-form").onsubmit = sendEmail;
