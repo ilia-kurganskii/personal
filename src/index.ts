1d19ac4aaa757d43267e4b38abf3024cfc1a669f
Ilya <ilya.kurganskii@gmail.com>
2020-10-10 01:01:42 +0400

    style: start to update design

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,10 @@ import "./css/normalize.css";
 import "./css/typography.css";
 import "./css/variables.scss";
 import "./css/sections.scss";
+import "./css/background.scss";
 
 import "./partials/header/header.ts";
 import "./partials/footer/footer.scss";
-
-import "./partials/sections/contact/contact";
-import "./partials/sections/contact/contact.scss";
 
 import "./partials/sections/main/main.scss";
 import "./partials/sections/career/career.scss";
